f46e47f7e545dbe4db2c36fcc4bf17a2cd9061d7
Joao Gabriel Carvalho <jg@jgcarv.com>
2022-02-23 09:42:15 -0500

    feat: Add interface and expose token addresses (#2)
    * added interface and exposed token addresses
    
    * fix: formatting

---
+++ contracts/interfaces/IMigrator.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.7;
+
+interface IMigrator {
+
+    /**
+     *  @dev    Get address of oldToken.  
+     *  @return oldToken_ The address of new token.
+     */
+    function oldToken() external view returns (address oldToken_);
+
+    /**
+     *  @dev    Get address of newToken.  
+     *  @return newToken_ The address of new token.
+     */
+    function newToken() external view returns (address newToken_);
+
+    /**
+     *  @dev    Exchange the oldToken for the same amount of newToken.
+     *  @param  amount_ The amount of oldToken to swap for newToken.
+     */
+    function migrate(uint256 amount_) external;
+
+}
