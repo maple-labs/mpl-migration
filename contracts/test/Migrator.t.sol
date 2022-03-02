3a14e7255edce0f77ebb02a50123f785b77bf420
Erick <erick@maple.finance>
2022-03-01 19:42:31 -0500

    fix: update MockERC20 path

--- contracts/test/Migrator.t.sol
+++ contracts/test/Migrator.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.7;
 
 import { TestUtils } from "../../modules/contract-test-utils/contracts/test.sol";
 
-import { MockERC20 } from "../../modules/erc20/src/test/mocks/MockERC20.sol";
+import { MockERC20 } from "../../modules/erc20/contracts/test/mocks/MockERC20.sol";
 
 import { Migrator } from "../Migrator.sol";
 
