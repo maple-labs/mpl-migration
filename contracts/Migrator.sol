a63ebf4896e8959f50e886f50d3f2bede7ad406e
0xfarhaan <59924029+0xfarhaan@users.noreply.github.com>
2024-06-26 12:01:04 +0100

    feat: Update interface for old/new token amounts

--- contracts/Migrator.sol
+++ contracts/Migrator.sol
@@ -29,18 +29,18 @@ contract Migrator is IMigrator {
         tokenSplitScalar = scalar_;
     }
 
-    function migrate(uint256 amount_) external override returns (uint256 migratedAmount_) {
-        migratedAmount_ = migrate(msg.sender, amount_);
+    function migrate(uint256 oldTokenAmount_) external override returns (uint256 newTokenAmount_) {
+        newTokenAmount_ = migrate(msg.sender, oldTokenAmount_);
     }
 
-    function migrate(address owner_, uint256 amount_) public override returns (uint256 migratedAmount_) {
-        require(active,                "M:M:INACTIVE");
-        require(amount_ != uint256(0), "M:M:ZERO_AMOUNT");
+    function migrate(address owner_, uint256 oldTokenAmount_) public override returns (uint256 newTokenAmount_) {
+        require(active,                        "M:M:INACTIVE");
+        require(oldTokenAmount_ != uint256(0), "M:M:ZERO_AMOUNT");
 
-        migratedAmount_ = amount_ * tokenSplitScalar;
+        newTokenAmount_ = oldTokenAmount_ * tokenSplitScalar;
 
-        require(ERC20Helper.transferFrom(oldToken, owner_, address(this), amount_), "M:M:TRANSFER_FROM_FAILED");
-        require(ERC20Helper.transfer(newToken, owner_, migratedAmount_),            "M:M:TRANSFER_FAILED");
+        require(ERC20Helper.transferFrom(oldToken, owner_, address(this), oldTokenAmount_), "M:M:TRANSFER_FROM_FAILED");
+        require(ERC20Helper.transfer(newToken, owner_, newTokenAmount_),                    "M:M:TRANSFER_FAILED");
     }
 
     function setActive(bool active_) external override {
